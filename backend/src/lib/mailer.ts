ce10bd4e62c905d1a817198b4fef6929d7c7c122
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2026-03-06 21:35:22 -0600

    otro cambio para saber errores

--- backend/src/lib/mailer.ts
+++ backend/src/lib/mailer.ts
@@ -6,14 +6,18 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
+  connectionTimeout: 5000,
+  greetingTimeout: 5000,
+  socketTimeout: 5000,
 });
 
 export const sendVerificationEmail = async (email: string, code: string) => {
-  await transporter.sendMail({
-    from: `"Rumbo" <${process.env.GMAIL_USER}>`,
-    to: email,
-    subject: "Verifica tu cuenta en Rumbo",
-    html: `
+  try {
+    await transporter.sendMail({
+      from: `"Rumbo" <${process.env.GMAIL_USER}>`,
+      to: email,
+      subject: "Verifica tu cuenta en Rumbo",
+      html: `
       <div style="font-family: sans-serif; max-width: 480px; margin: 0 auto; padding: 32px;">
         <h2 style="color: #5a1236;">Bienvenido a Rumbo 🎓</h2>
         <p>Tu código de verificación es:</p>
@@ -23,7 +27,11 @@ export const sendVerificationEmail = async (email: string, code: string) => {
         <p style="color: #888; font-size: 13px;">Expira en 15 minutos.</p>
       </div>
     `,
-  });
+    });
+    console.log("Email enviado");
+  } catch (error) {
+    console.log("Error enviando email, ", error);
+  }
 };
 
 export const sendResetEmail = async (email: string, token: string) => {
