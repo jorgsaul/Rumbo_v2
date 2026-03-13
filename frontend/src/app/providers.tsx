ebd3960e1e1adb1b16b8149f1fb131810dcb58cf
jorgsaul <jvelazquezp2301@gmail.com>
2026-03-12 19:05:24 -0600

    agregar al provider

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import ToastProvider from "@/context/ToastContext";
+import ConfirmationProvider from "@/context/ConfirmationContext";
+import ConfirmModal from "@/components/shared/ConfirmModal";
+
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      <ToastProvider>{children}</ToastProvider>
+      <ToastProvider>
+        <ConfirmationProvider>
+          {children}
+          <ConfirmModal />
+        </ConfirmationProvider>
+      </ToastProvider>
     </GoogleOAuthProvider>
   );
 }
