026c4c708b26919d581d9f6f3920af8de4127bd6
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2026-04-17 00:55:08 -0600

    solo dejo localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   }),
 );
