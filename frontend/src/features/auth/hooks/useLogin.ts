66d624b9716125945e94dc032916c87539a04979
jorgsaul <jvelazquezp2301@alumno.ipn.mx>
2026-04-17 00:08:53 -0600

    evitar redireccionamiento

--- frontend/src/features/auth/hooks/useLogin.ts
+++ frontend/src/features/auth/hooks/useLogin.ts
@@ -23,7 +23,7 @@ export default function useLogin() {
           secure: process.env.NODE_ENV === "production",
           sameSite: "strict",
         });
-        router.push("/feed");
+        //roSuter.push("/feed");
       } else {
         setError(res.message);
       }
